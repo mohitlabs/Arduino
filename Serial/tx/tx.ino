abe0356bf44d18c61c531b926990caeed2e2589b
MohitLabs <97015840+mohitlabs@users.noreply.github.com>
2022-03-18 01:21:47 +0530

    Add files via upload

---
+++ Serial/tx/tx.ino
@@ -0,0 +1,19 @@
+const int swPin=9;
+void setup()
+{
+	Serial.begin(9600);
+	pinMode(swPin, INPUT);
+}
+void loop()
+{
+	if(digitalRead(swPin))
+	{
+		Serial.write('M');
+		delay(1000);
+	}
+	else
+	{
+		Serial.write('0');
+		delay(200);
+	}
+}
